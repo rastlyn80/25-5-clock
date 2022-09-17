a409a20da887d935b3c117f245f49a0c42ad7c9b
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-17 23:29:34 +0100

    Created Incremental Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Incremental from "./Incremental";
 
 const App = () => {
-  return <div>APP COMPONENT</div>;
+  return (
+    <div>
+      APP COMPONENT
+      <Incremental />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Incremental.js
@@ -0,0 +1,11 @@
+// This Component will be reusable to represent both the Break Length and the Session Length controllers
+
+import React from "react";
+
+class Incremental extends React.Component {
+  render() {
+    return <div>INCREMENTAL COMPONENT</div>;
+  }
+}
+
+export default Incremental;
