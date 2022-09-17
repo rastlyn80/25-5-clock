ce30bc6f2dd6a61bf8b5584155e64b763c8c205b
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-18 00:32:25 +0100

    More styling done

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,11 @@ class App extends React.Component {
     return (
       <div class="container">
         <div className="main">
-          APP COMPONENT
-          <Incremental title="Break Length" initialValue="5" callBack="" />
+          <h2>25 + 5 Clock</h2>
+          <div className="arrows">
+            <Incremental title="Break Length" initialValue="5" callBack="" />
+            <Incremental title="Break Length" initialValue="5" callBack="" />
+          </div>
         </div>
       </div>
     );
--- src/components/Incremental.js
+++ src/components/Incremental.js
@@ -1,10 +1,21 @@
 // This Component will be reusable to represent both the Break Length and the Session Length controllers
 
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 class Incremental extends React.Component {
   render() {
-    return <div>{this.props.title}</div>;
+    return (
+      <div className="incremental">
+        {this.props.title}
+        <div className="arrows">
+          <FontAwesomeIcon icon={faArrowUp} />
+          {this.props.initialValue}
+          <FontAwesomeIcon icon={faArrowDown} />
+        </div>
+      </div>
+    );
   }
 }
 
