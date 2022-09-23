8fb9b9811519cb3d1ae872a03ab0814580663f1f
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-23 19:21:20 +0100

    Finished Styling the Controls Component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faRefresh } from "@fortawesome/free-solid-svg-icons";
 
 class Controls extends React.Component {
   render() {
-    return <div>CONTROLS</div>;
+    return (
+      <div className="arrows" style={{ width: "50%", margin: "auto" }}>
+        <div id="start_stop">
+          <FontAwesomeIcon icon={faPlay} />
+          <FontAwesomeIcon icon={faPause} />
+        </div>
+        <div id="reset">
+          <FontAwesomeIcon icon={faRefresh} />
+        </div>
+      </div>
+    );
   }
 }
 
