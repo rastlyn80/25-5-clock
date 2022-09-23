612fee41bb754d3d823db63cc1c889bf63028526
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-23 19:29:24 +0100

    Added credits + more styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import SessionIncremental from "./SessionIncremental";
 import BreakIncremental from "./BreakIncremental";
 import CountDown from "./CountDown";
 import Controls from "./Controls";
+import Credits from "./Credits";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <div className="main">
-          <h2>25 + 5 Clock</h2>
+          <h2 className="green-caption">25 + 5 CLOCK</h2>
           <div className="arrows">
             <SessionIncremental />
             <BreakIncremental />
@@ -18,6 +19,7 @@ class App extends React.Component {
             <CountDown />
           </div>
           <Controls />
+          <Credits />
         </div>
       </div>
     );
---
+++ src/components/Credits.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Credits = () => {
+  return (
+    <div className="credits">
+      <div className="green-caption">Designed and Coded by</div>
+      Gabriel Baldock
+    </div>
+  );
+};
+
+export default Credits;
